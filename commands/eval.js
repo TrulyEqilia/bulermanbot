d7a1e98b8503f734d8c568595419684d55da8c3d
Eqilia <trulyeqilia@gmail.com>
2024-05-20 07:00:35 +0200

    chore: Add meh-quality eval command
    Signed-off-by: Eqilia <trulyeqilia@gmail.com>

---
+++ commands/eval.js
@@ -0,0 +1,28 @@
+const util = require('node:util');
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	name: 'eval',
+	description: 'ewaluator',
+	data: new SlashCommandBuilder().
+		addStringOption((opt) => opt.setName('kod').setDescription('kod').setRequired(true)),
+	async execute(i)
+	{
+		if (i.user.id !== '1125528136524775444')
+		if (i.user.id !== '1084489833117917254')
+		if (i.user.id !== '1202021421677826052')
+			return await i.reply('OwO');
+
+		const input = i.options.get('kod');
+		let output;
+
+		try {
+			output = await util.inspect(eval(input));
+		} catch (e) {
+			output = util.inspect(e);
+		}
+
+		return i.reply(output);
+	}
+}
