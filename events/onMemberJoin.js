5a124215f1bfb274d38584ac5699a226c97225c0
@f908 <48841429+f908@users.noreply.github.com>
2024-05-20 16:04:42 +0200

    Change member join message

--- events/onMemberJoin.js
+++ events/onMemberJoin.js
@@ -5,6 +5,6 @@ module.exports = {
 		if (member.guild.id !== '1206262266953998336') return;
 		const chat = member.client.channels.cache.get('1241870376934441173');
 
-		chat.send(`✨ ${member.user.username} (${member.user.globalName}, <@${member.user.id}>) ✨ dołączył`);
+		chat.send(`✨ ${member.user.username} (${member.user.globalName}, <@${member.user.id}>) ✨ dołączył/a`);
 	}
 }
