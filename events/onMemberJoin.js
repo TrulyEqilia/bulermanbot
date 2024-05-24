4dc356b60a0bc729afb22db1e258e181a71a2e5a
Eqilia <trulyeqilia@gmail.com>
2024-05-24 17:00:46 +0200

    chore: Remove onMember events
    They aren't used anymore, and they were locally disabled anyways. It's
    time to remove them entirely.
    
    Signed-off-by: Eqilia <trulyeqilia@gmail.com>

--- events/onMemberJoin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'guildMemberAdd',
-	async execute(member) {
-		if (member.guild.id !== '1206262266953998336') return;
-		const chat = member.client.channels.cache.get('1241870376934441173');
-
-		chat.send(`✨ ${member.user.username} (${member.user.globalName}, <@${member.user.id}>) ✨ dołączył`);
-	}
-}
