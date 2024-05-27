9ee7a7f3b0ccb4dcefc9940cdd9fc0d684de87c2
Eqilia <trulyeqilia@gmail.com>
2024-05-27 09:17:58 +0200

    chore: Add enumerator variations in Polish, fixes.
    globalThis->moderative->warnUser(3) originally had code from warn.js,
    which used victim.value instead of id.
    
    Add enumerator variations in Polish for the warn.js command, just so it
    looks nicer.
    
    Signed-off-by: Eqilia <trulyeqilia@gmail.com>

--- dbModule.js
+++ dbModule.js
@@ -32,12 +32,13 @@ globalThis.profile = {
 globalThis.moderative = {
 	warnUser(id)
 	{
-		//
-		const profile = globalThis.db.get(`uprf_${victim.value}`);
+		const profile = globalThis.db.get(`uprf_${id}`);
 
 		profile.warnings++;
 
-		globalThis.db.set(`uprf_${victim.value}`, profile);
+		globalThis.db.set(`uprf_${id}`, profile);
+
+		return profile.warnings;
 	}
 }
 
@@ -46,3 +47,37 @@ module.exports = {
 	profile: globalThis.profile,
 	moderative: globalThis.moderative
 }
+
+globalThis.odmianyCyfr = [
+	"zerowe (co??)",
+	"pierwsze",
+	"drugie",
+	"trzecie",
+	"czwarte",
+	"piąte",
+	"szóste",
+	"siódme",
+	"ósme",
+	"dziewiąte",
+	"dziesiąte",
+	"jedenaste",
+	"dwunaste",
+	"trzynaste",
+	"czternaste",
+	"piętnaste",
+	"szesnaste",
+	"siedemnaste",
+	"osiemnaste",
+	"dziewiętnaste",
+	"dwudzieste",
+	"dwudzieste pierwsze",
+	"dwudzieste drugie",
+	"dwudzieste trzecie",
+	"dwudzieste czwarte",
+	"dwudzieste piąte",
+	"dwudzieste szóste",
+	"dwudzieste siódme",
+	"dwudzieste ósme",
+	"dwudzieste dziewiąte",
+	"trzydzieste"
+]
